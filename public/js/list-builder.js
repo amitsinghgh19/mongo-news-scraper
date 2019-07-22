795bf0c54a994378d392a664116ed605bbd9d370
amitsinghgh19 <prasadmanigh19>
2019-07-22 18:50:53 -0400

    removed sendgrid api key

--- public/js/list-builder.js
+++ public/js/list-builder.js
@@ -33,15 +33,4 @@ $(document).ready(function() {
         });
     }
 
-    // const sgMail = require('@sendgrid/mail');
-    // sgMail.setApiKey(process.env.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);
-    // const msg = {
-    // to: 'test@example.com',
-    // from: 'test@example.com',
-    // subject: 'Sending with Twilio SendGrid is Fun',
-    // text: 'and easy to do anywhere, even with Node.js',
-    // html: '<strong>and easy to do anywhere, even with Node.js</strong>',
-    // };
-    // sgMail.send(msg);
-
 });
